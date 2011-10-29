b8470f8ec966f9d0fcd8c293986d5c3c5f0d3fc9
misha.seltzer <misha.seltzer@gmail.com>
2011-10-29 13:45:52 +0000

    Added NotifyListeners to the interface.

--- android/src/com/google/iamnotok/LocationTracker.java
+++ android/src/com/google/iamnotok/LocationTracker.java
@@ -6,11 +6,13 @@ public interface LocationTracker {
 	public interface Listener {
 		void notifyNewLocation(Location location);
 	}
-	void activate();
-	void deactivate();
+	public void activate();
+	public void deactivate();
 
 	/**
 	 * Register a listener to be called when location is updated by a substantial distance or better accuracy
 	 */
-	void registerListenersForBetterLocation(Listener listener);
+	public void registerListenersForBetterLocation(Listener listener);
+
+	public void notifyListeners();
 }
