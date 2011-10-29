b8fc722370b5b05bb304fd09b4e4071857fe3388
igal1987 <igal1987@gmail.com>
2011-10-29 14:58:16 +0000

    beautify

--- android/src/com/google/iamnotok/utils/LimitedQueue.java
+++ android/src/com/google/iamnotok/utils/LimitedQueue.java
@@ -3,23 +3,29 @@ package com.google.iamnotok.utils;
 import java.util.LinkedList;
 
 
+/**
+ * This queue has a threshold and only saves the last [threshold] items in it.
+ * 
+ * @param <E> The type of the elements in the queue.
+ */
 public class LimitedQueue<E> extends LinkedList<E> {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -3865727704298539447L;
-	private int maxSize = 0;
-	//private E first Item = null;
 	
-	public LimitedQueue(int maxSizeP){
-		this.maxSize = maxSizeP;
+	private int maxSize = 0;
+	
+	/**
+	 * Constructor.
+	 * 
+	 * @param maxSize The threshold of this queue.
+	 */
+	public LimitedQueue(int maxSize){
+		this.maxSize = maxSize;
 	}
 
 	@Override
 	public boolean offer(E item){
-		if (this.size() == maxSize){
-			this.remove();
+		if (size() == maxSize){
+			remove();
 		}
 		
 		return super.offer(item);
