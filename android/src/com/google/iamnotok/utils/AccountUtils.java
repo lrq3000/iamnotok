153bec5d20e5d7e9a15debc851335bcf9682c85a
nirsof <nirsof@gmail.com>
2012-12-01 16:24:49 +0000

    Start Preferences class - handling only selected account currrently

--- android/src/com/google/iamnotok/utils/AccountUtils.java
+++ android/src/com/google/iamnotok/utils/AccountUtils.java
@@ -10,10 +10,14 @@ import android.preference.PreferenceManager;
 import android.provider.ContactsContract.CommonDataKinds.Email;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 
+import com.google.iamnotok.Preferences;
 import com.google.iamnotok.R;
 
 public class AccountUtils {
+	private static final String LOG_TAG = "AccountUtils";
+	
 	private final Context context;
 
 	public AccountUtils(Context context) {
@@ -26,21 +30,30 @@ public class AccountUtils {
 
 	private Account getSelectedAccount() {
 		// First try to get the selected account from the preferences.
-		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this.context);
-		String accountName = pref.getString(
-				context.getString(R.string.select_account_list), "");
+		Preferences pref = new Preferences(this.context);
+				
+		String accountName = pref.getAccountName();
 		Account[] accounts = AccountManager.get(context).getAccountsByType("com.google");
 		for (Account account : accounts) {
 			if (account.name != null && account.name.equals(accountName)) {
+				Log.d(LOG_TAG, "using prefered account: " + accountName);
 				return account;
 			}
 		}
 
 		// If we got here, then we didn't find the account in the preferences.
 		// This probably means that the user removed the account after selecting it.
-		// In this case, we just return the first available account, if any.
+		// In this case, we clear the preferred name and return the first available 
+		// account, if any.
 		// TODO: Should we notify the user here?
+		
+		if (!accountName.equals("")) {
+			Log.d(LOG_TAG, "no such account: " + accountName + " clearing preferred account");
+			pref.setAccountName("");
+		}
+		
 		if (accounts.length > 0) {
+			Log.d(LOG_TAG, "using default gmail account: " + accounts[0].name);
 			return accounts[0];
 		}
 
@@ -48,9 +61,11 @@ public class AccountUtils {
 		// NOTE: This can only be used to get the name from.
 		accounts = AccountManager.get(context).getAccounts();
 		if (accounts.length > 0) {
+			Log.d(LOG_TAG, "using default account: " + accounts[0].name);
 			return accounts[0];
 		}
 
+		Log.d(LOG_TAG, "using dummy account");
 		return new Account("Unidentified User", "com.dummy");
 	}
 
